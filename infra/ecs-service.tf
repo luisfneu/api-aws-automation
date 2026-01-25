77b56313ff60a26290b445eb5dad8298a7bdfe50
Luis Fernando Neu <luisneu@gmail.com>
2026-01-24 23:03:54 -0300

    terraform start

---
+++ infra/ecs-service.tf
@@ -0,0 +1,21 @@
+resource "aws_ecs_service" "api" {
+  name            = "api"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.api.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = aws_subnet.private[*].id
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.api.arn
+    container_name   = "api"
+    container_port   = 3000
+  }
+
+  depends_on = [aws_lb_listener.http]
+}
